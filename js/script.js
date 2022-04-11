736738a249355b8f9ac7428b3a96e812fd6af017
Federico Errecalde <federicoerrecalde@gmail.com>
2022-04-11 12:10:17 -0300

    empezando About

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ menuBtn.onclick = () =>{
 }
 
 var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
